1aa865d0e7bc67ba0079a857ffb469319eab284f
marini-pietro <pietromarinivr2006@gmail.com>
2026-04-03 13:24:41 +0200

    Upgrades docker compose wrapper compose scripts to use docker compose  V2 command

--- docker/run_compose.bat
+++ docker/run_compose.bat
@@ -5,6 +5,7 @@ REM Go to project root (parent of this script's directory)
 cd /d "%~dp0.."
 
 REM Use .env from project root and compose file from docker/
-docker-compose --env-file .env -f docker\docker-compose.yml %*
+docker compose --env-file .env -f docker\docker-compose.yml %*
+REM # docker-compose --env-file .env -f docker/docker-compose.yml "$@" (for Docker compose v1)
 
 endlocal
--- docker/run_compose.sh
+++ docker/run_compose.sh
@@ -6,4 +6,5 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR/.."
 
 # Use .env from project root and compose file from docker/
-docker-compose --env-file .env -f docker/docker-compose.yml "$@"
+docker compose --env-file .env -f docker/docker-compose.yml "$@"
+# docker-compose --env-file .env -f docker/docker-compose.yml "$@" (for Docker compose v1)
